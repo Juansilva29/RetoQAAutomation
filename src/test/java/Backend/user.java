d81991001a815b9bcc325eec7491e45f3f4bb642
juan.fsilva <hammerpipo21@hotmail.com>
2024-05-14 07:24:10 -0500

    update user.java

--- src/test/java/Backend/user.java
+++ src/test/java/Backend/user.java
@@ -17,9 +17,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class user {
+
+    String path = "classpath:backend/";
+    /*@Test
+    void testParallel(){
+        Results results =  Runner.path("classpath:backend").outputCucumberJson(true).tags("~@ignore").parallel(1);
+        generateReport(results.getReportDir());
+    }*/
+
     @Test
-
-
     Karate testSample(){
         String path = "classpath:backend/";
          return Karate.run(path+"user.feature",
@@ -28,13 +34,15 @@ public class user {
         ).relativeTo(getClass()).outputCucumberJson(true);
 
         //generateReport(results.getReportDir());
-    }
+        }
+     
+
 
     public static void generateReport(String karateOutputPath){
         Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[]{"json"}, true);
         List<String> jsonPaths = new ArrayList<>(jsonFiles.size());
         jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
-        Configuration config = new Configuration(new File("build"), "curso_karate");
+        Configuration config = new Configuration(new File("build"), "RetoQAAutomation");
         ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
         reportBuilder.generateReports();
     }
