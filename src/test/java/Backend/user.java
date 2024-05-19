b263bd797a8348cf165404d899a33fd85fc04361
Juansilva29 <hammerpipo21@hotmail.com>
2024-05-19 10:43:10 -0500

    delete imports

--- src/test/java/Backend/user.java
+++ src/test/java/Backend/user.java
@@ -1,17 +1,9 @@
 package Backend;
 
-import com.intuit.karate.Results;
-import com.intuit.karate.Runner;
+
 import com.intuit.karate.junit5.Karate;
 import com.intuit.karate.junit5.Karate.Test;
-import net.masterthought.cucumber.Configuration;
-import net.masterthought.cucumber.ReportBuilder;
-import org.apache.commons.io.FileUtils;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -23,9 +15,9 @@ public class user {
     @Test
     Karate testSample(){
         String path = "classpath:backend/";
-         return Karate.run(/*path+"user.feature",
+         return Karate.run(path+"user.feature",
                 path+"updateUser.feature",
-                path+"queryUser.feature",*/
+                path+"queryUser.feature",
                  path+"scenarioOutline.feature"
         ).relativeTo(getClass());
      }
